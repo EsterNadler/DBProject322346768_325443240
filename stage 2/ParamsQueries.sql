e82b1d680ab72032ba3afa8a04889e8dd4304b4e
Tzipora Maroko <116155777+TziporaMaroko@users.noreply.github.com>
2024-06-09 15:28:54 +0300

    Add files via upload

--- stage 2/ParamsQueries.sql
+++ stage 2/ParamsQueries.sql
@@ -4,29 +4,29 @@ FROM Person p
 JOIN Gym_Member gm ON p.PersonId = gm.PersonId
 JOIN Takes_Lesson tl ON gm.PersonId = tl.PersonId
 JOIN Lesson l ON tl.LessonId = l.LessonId AND tl.ClassId = l.ClassId
-WHERE EXTRACT(MONTH FROM p.BirthDate) = &<name="select month:" list="1,2,3,4,5,6,7,8,9,10,11,12" multiselect="true">
-  AND l.LessonDate >= ADD_MONTHS(SYSDATE, -12);--??? what about multiply
-
+WHERE EXTRACT(MONTH FROM p.BirthDate) IN (&<name="select month:" list="1,2,3,4,5,6,7,8,9,10,11,12" multiselect="true">)
+  AND l.LessonDate >= ADD_MONTHS(SYSDATE, -12);
+SELECT * FROM LESSON;
 --find the number of lessons a specific trainer conducted in a given month:  
 SELECT t.PersonId, EXTRACT(MONTH FROM l.LessonDate) AS LessonMonth, COUNT(l.LessonId) AS LessonCount
 FROM Trainer t
 JOIN Lesson l ON t.PersonId = l.PersonId
-WHERE t.PersonId = &<name="insert trainer id:" type="integer" hint="trainer id is number with 8 digits" required="true">
+WHERE t.PersonId = &<name="insert trainer id:" type="integer" list="select personID from TRAINER" hint="trainer id is number with 8 digits" 
+required="true">
   AND EXTRACT(MONTH FROM l.LessonDate) = &<name="select month:" list="1,2,3,4,5,6,7,8,9,10,11,12">
 GROUP BY t.PersonId, EXTRACT(MONTH FROM l.LessonDate);
 
 --selects all classes in a certain age range
 SELECT gc.ClassId, gc.ClassName, gc.MinAge, gc.MaxAge
 FROM Gym_Class gc
-WHERE gc.MinAge >= &<name="insert min age:" type="integer" default="0">  AND gc.MaxAge <= &<name="insert max age:" type="integer" default="99">;
-
+WHERE gc.MinAge >= &<name="insert min age:" type="integer" default="0">  
+AND gc.MaxAge <= &<name="insert max age:" type="integer" default="99">;
 
 -- Selects all lessons on a certain date and the number of participants in each one
 SELECT l.LessonId, l.LessonDate, COUNT(tl.PersonId) AS ParticipantCount
 FROM Lesson l
 LEFT JOIN Takes_Lesson tl ON l.LessonId = tl.LessonId AND l.ClassId = tl.ClassId
-WHERE l.LessonDate = TO_DATE(&<name="lesson_date" type="string">, 'dd/mm/yyyy')
-GROUP BY l.LessonId, l.LessonDate;--????????????????????????????? 
---trigger????
+WHERE trunc(l.LessonDate) = &<name="lesson_date" type=date>
+GROUP BY l.LessonId, l.LessonDate; 
 
 
--- stage 2/Queries.sql
+++ stage 2/Queries.sql
@@ -1,35 +1,58 @@
-
--------------------SELECT QUERIES-----------------------
-
---Select all lessons with their corresponding class name, room number, and lesson date, sorted by lesson date:
-SELECT GC.ClassName, L.RoomNum, L.LessonDate
+--Select class name, room number, lesson date, and number of participants for each lesson that had at least 
+--6 participants, ordered by the lesson date.
+SELECT GC.ClassName, L.RoomNum, L.LessonDate, Participation.NumParticipants
 FROM Lesson L
 JOIN Gym_Class GC ON L.ClassId = GC.ClassId
+JOIN (SELECT TL.LessonId, TL.ClassId, COUNT(TL.PersonId) AS NumParticipants
+     FROM Takes_Lesson TL
+     GROUP BY TL.LessonId, TL.ClassId
+     HAVING COUNT(TL.PersonId) >= 6
+    ) Participation 
+    ON L.LessonId = Participation.LessonId AND L.ClassId = Participation.ClassId
 ORDER BY L.LessonDate;
 
+--Select all members who have taken all available classes:
+SELECT P.FirstName,P.LastName
+FROM Gym_Member GM
+JOIN Person P ON GM.PersonId = P.PersonId
+WHERE NOT EXISTS (
+        SELECT GC.ClassId
+        FROM Gym_Class GC
+        WHERE NOT EXISTS (
+            SELECT 1
+            FROM Takes_Lesson TL
+            WHERE TL.ClassId = GC.ClassId AND TL.PersonId = GM.PersonId
+        )
+    );
+
 --Select class names and their average ratings, ordered by rating
-SELECT GC.ClassName, AVG(L.Rating) AS AvgRating
-FROM Lesson L
-JOIN Gym_Class GC ON L.ClassId = GC.ClassId
-GROUP BY GC.ClassName
-ORDER BY AvgRating DESC;
+SELECT ClassDetails.ClassName, AvgRating
+FROM (SELECT GC.ClassId,GC.ClassName,AVG(L.Rating) AS AvgRating
+      FROM Lesson L
+      JOIN Gym_Class GC ON L.ClassId = GC.ClassId
+      GROUP BY GC.ClassId, GC.ClassName
+    ) ClassDetails
+WHERE AvgRating <= ALL (
+      SELECT AVG(L.Rating) AS AvgRating
+      FROM Lesson L
+      JOIN Trainer T ON L.PersonId = T.PersonId
+      WHERE T.PERSONID IN (SELECT L.PersonID
+                           FROM LESSON L
+                           WHERE L.CLASSID = ClassDetails.ClassId)
+      GROUP BY T.PersonId
+    );
 
--- Select all trainers along with their experience and the number of classes they teach
-SELECT T.PersonId, P.FirstName, P.LastName, T.Experience, COUNT(L.ClassId) AS NumClasses
-FROM Trainer T
-JOIN Person P ON T.PersonId = P.PersonId
-JOIN Lesson L ON T.PersonId = L.PersonId
-GROUP BY T.PersonId, P.FirstName, P.LastName, T.Experience;
+-- Select the experience level and the count of trainers who have that experience level.
+SELECT RES.Experience, COUNT(RES.PersonId) AS NumTainers
+FROM (SELECT T.PersonId, P.FirstName, P.LastName, T.Experience, COUNT(L.ClassId) AS NumClasses
+      FROM Trainer T
+      JOIN Person P ON T.PersonId = P.PersonId
+      JOIN Lesson L ON T.PersonId = L.PersonId
+      GROUP BY T.PersonId, P.FirstName, P.LastName, T.Experience
+      ORDER BY T.Experience) RES
+GROUP BY RES.Experience;
 
--- Select all members who joined the gym in the last month
-SELECT p.FirstName, p.LastName, gm.JoinDate
-FROM Gym_Member gm
-JOIN Person p ON gm.PersonId = p.PersonId
-WHERE gm.JoinDate >= ADD_MONTHS(SYSDATE, -1) AND  gm.JoinDate <= SYSDATE;
-
--------------------UPDATE QUERIES-----------------------
-
--- Update trainer experience if the trainer taught at least 1 lesson on the last year
+-- Update trainer experience if the trainer tought at least 1 lesson on the last year
 UPDATE Trainer
 SET Experience = Experience + 1
 WHERE PersonId IN (
@@ -49,23 +72,37 @@ WHERE PersonId NOT IN (
     WHERE l.LessonDate >= ADD_MONTHS(SYSDATE, -12)
 );
 
--------------------DELETE QUERIES-----------------------
-
 -- Step 1: Drop existing foreign key constraint (if it exists)
 ALTER TABLE Lesson
 DROP CONSTRAINT SYS_C00711881;
 
+ALTER TABLE Takes_Lesson 
+DROP CONSTRAINT SYS_C00711887;
+
 ALTER TABLE Takes_Lesson
 DROP CONSTRAINT SYS_C00711886;
+
+ALTER TABLE GYM_MEMBER_MEDICAL_CONSTRAINS
+DROP CONSTRAINT SYS_C00709992;
+
 -- Step 2: Add foreign key constraint with ON DELETE CASCADE
 ALTER TABLE Lesson
 ADD CONSTRAINT SYS_C00711881
 FOREIGN KEY (PersonId) REFERENCES Trainer(PersonId) ON DELETE CASCADE;
 
 ALTER TABLE Takes_Lesson
+ADD CONSTRAINT SYS_C00711887
+FOREIGN KEY (PersonId)
+REFERENCES GYM_MEMBER(PersonId) ON DELETE CASCADE;
+
+ALTER TABLE Takes_Lesson
 ADD CONSTRAINT SYS_C00711886
 FOREIGN KEY (LessonId, ClassId)
 REFERENCES Lesson(LessonId, ClassId) ON DELETE CASCADE;
+
+ALTER TABLE GYM_MEMBER_MEDICAL_CONSTRAINS
+ADD CONSTRAINT SYS_C00709992
+FOREIGN KEY (PersonId) REFERENCES GYM_MEMBER(PersonId) ON DELETE CASCADE;
 
 --Delete all trainers with less than one year of experience who have not conducted any lessons in the past year
 DELETE FROM Trainer
